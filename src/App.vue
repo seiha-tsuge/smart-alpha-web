5e3fa663702ec214d900f4c5673ee397d506491b
zawa24-ask <asako.nishizawa2438@gmail.com>
2021-07-07 14:46:34 +0000

    feat: グリッドレイアウト作成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="container">
-    <header></header>
-    <div>
-      <div class="sidebar"></div>
-      <main></main>
-    </div>
+    <header class="header">あ</header>
+    <div class="sidebar">い</div>
+    <main class="main">う</main>
   </div>
 </template>
 
@@ -15,3 +13,27 @@ export default defineComponent({
   name: 'App',
 });
 </script>
+
+<style scoped>
+.container {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  grid-template-columns: auto;
+  grid-template-rows: 50px 1fr;
+}
+
+.header {
+  background-color: red;
+  grid-column-start: 1;
+  grid-column-end: 3;
+}
+
+.sidebar {
+  background-color: yellow;
+}
+
+.main {
+  background-color: blue;
+}
+</style>
