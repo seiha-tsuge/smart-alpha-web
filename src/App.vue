a4d22f10f562a926435678ad78696169c5aba937
zawa24-ask <asako.nishizawa2438@gmail.com>
2021-07-28 14:57:03 +0000

    feat: レイアウトコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,25 @@
 <template>
-  <div class="container">
-    <header class="header">あ</header>
-    <aside class="sidebar">い</aside>
-    <main class="main">う</main>
-  </div>
+  <LayoutContainer>
+    <LayoutHeader></LayoutHeader>
+    <LayoutSidebar></LayoutSidebar>
+    <LayoutMain></LayoutMain>
+  </LayoutContainer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import LayoutContainer from './components/layout/LayoutContainer.vue';
+import LayoutHeader from './components/layout/LayoutHeader.vue';
+import LayoutSidebar from './components/layout/LayoutSidebar.vue';
+import LayoutMain from './components/layout/LayoutMain.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    LayoutContainer,
+    LayoutHeader,
+    LayoutSidebar,
+    LayoutMain,
+  },
 });
 </script>
-
-<style scoped>
-.container {
-  display: grid;
-  grid-template:
-    'header header' 56px
-    'sidebar main' 1fr / 240px 1fr;
-  width: 100vw;
-  height: 100vh;
-}
-
-.header {
-  grid-area: header;
-  background-color: red;
-}
-
-.sidebar {
-  grid-area: sidebar;
-  background-color: yellow;
-}
-
-.main {
-  grid-area: main;
-  background-color: blue;
-}
-
-@media screen and (max-width: 576px) {
-  .container {
-    grid-template:
-      'header' 56px
-      'main' 1fr / 1fr;
-  }
-
-  .sidebar {
-    position: fixed;
-    top: 0;
-    z-index: var(--z-index-sidebar);
-    width: 75%;
-    height: 100vh;
-  }
-}
-</style>
---
+++ src/components/layout/LayoutSidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside :class="$style.sidebar">
+    <slot />
+  </aside>
+</template>
+
+<style module>
+.sidebar {
+  grid-area: sidebar;
+  background-color: yellow;
+}
+
+@media screen and (max-width: 576px) {
+  .sidebar {
+    position: fixed;
+    top: 0;
+    z-index: var(--z-index-sidebar);
+    width: 75%;
+    height: 100vh;
+  }
+}
+</style>
