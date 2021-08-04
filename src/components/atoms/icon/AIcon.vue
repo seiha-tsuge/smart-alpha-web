dfe5d84a6605eae15e5221fcedb76ff942af636e
Reon Horigami <excalibr28@gmail.com>
2021-08-05 00:19:53 +0900

    ボタンとアイコンの作成

---
+++ src/components/atoms/icon/AIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <i>
+    <slot />
+  </i>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AIcon',
+  props: {
+    color: {
+      type: String,
+      default: null,
+    },
+  },
+  setup() {
+    this.color === colors.color;
+  },
+});
+</script>
+
+<style module="colors">
+.primary {
+  color: var(--color-primary);
+}
+</style>
