b51e36c0e32b3d9743e04d0d6bb5f784e15460c9
Takuya-Akutsu <potatop0103@gmail.com>
2021-08-07 00:29:56 +0900

    feat:アイコンの作成

--- src/components/atoms/icon/AIcon.vue
+++ src/components/atoms/icon/AIcon.vue
@@ -1,7 +1,7 @@
 <template>
-  <i>
-    <slot />
-  </i>
+  <i class="material-icons-outlined" :class="[colors[color], size[size]]">{{
+    icon
+  }}</i>
 </template>
 
 <script lang="ts">
@@ -10,13 +10,18 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'AIcon',
   props: {
+    icon: {
+      type: String,
+      required: true,
+    },
     color: {
       type: String,
-      default: null,
+      default: 'default',
     },
-  },
-  setup() {
-    this.color === colors.color;
+    size: {
+      type: String,
+      default: 'lg',
+    },
   },
 });
 </script>
@@ -24,5 +29,20 @@ export default defineComponent({
 <style module="colors">
 .primary {
   color: var(--color-primary);
+}
+.default {
+  color: var(--color-gray-darker);
+}
+</style>
+
+<style module="size">
+.sm {
+  font-size: 24px;
+}
+.md {
+  font-size: 36px;
+}
+.lg {
+  font-size: 48px;
 }
 </style>
