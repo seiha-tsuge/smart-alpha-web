dba18a57e340be664925f8a654db36fe1586d8cb
Seiha Tsuge <49358142+seiha-tsuge@users.noreply.github.com>
2021-08-18 22:16:10 +0900

    feat: Atom Iconコンポーネントの作成 (#25)

---
+++ src/components/atoms/button/AButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AButton',
+  props: {},
+  setup() {},
+});
+</script>
---
+++ src/components/atoms/icon/AIcon.vue
@@ -0,0 +1,52 @@
+<template>
+  <i class="material-icons-outlined" :class="[colors[color], sizes[size]]">
+    {{ icon }}
+  </i>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+import { defineComponent } from 'vue';
+
+type Color = 'primary' | 'default';
+type Size = 'default' | 'small' | 'large';
+
+export default defineComponent({
+  name: 'AIcon',
+  props: {
+    icon: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    color: {
+      type: String as PropType<Color>,
+      default: 'default',
+    },
+    size: {
+      type: String as PropType<Size>,
+      default: 'small',
+    },
+  },
+});
+</script>
+
+<style module="colors">
+.primary {
+  color: var(--color-primary);
+}
+.default {
+  color: var(--color-gray-darker);
+}
+</style>
+
+<style module="sizes">
+.small {
+  font-size: 1rem !important;
+}
+.default {
+  font-size: 1.5rem;
+}
+.large {
+  font-size: 2.25rem !important;
+}
+</style>
