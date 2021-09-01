ca5f084fd5d5826e85884f5f7291738626c53003
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-09-01 23:10:30 +0900

    refactor: createAppを変数に格納

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,7 @@ import { router } from './router';
 
 import './assets/styles/index.css';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+
+app.mount('#app');
