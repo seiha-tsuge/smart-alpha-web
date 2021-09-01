da9e114771c481dedbc4edd4689ec672319df716
Reon Horigami <84082312+reon-horigami@users.noreply.github.com>
2021-09-01 21:55:27 +0900

    feat: Headerの作成 (#28)

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <LayoutHeader :class="$style.header">
+    <AIcon icon="menu" />
+    <div>login</div>
+  </LayoutHeader>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LayoutHeader from '../layout/LayoutHeader.vue';
+import AIcon from '../atoms/icon/AIcon.vue';
+
+export default defineComponent({
+  components: {
+    LayoutHeader,
+    AIcon,
+  },
+});
+</script>
+
+<style module>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
