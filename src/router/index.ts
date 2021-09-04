1879e42f7777c725cf7bfb955bcb2bc1da52ac50
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-09-05 01:09:23 +0900

    feat: historyにcreateWebHistoryを設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const About = { template: '<div>About</div>' };
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [{ path: '/about', component: About }],
 });
