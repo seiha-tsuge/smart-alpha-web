000daad3bb9db6985014d8a2c2a9cb42b4208df2
Reon Horigami <excalibr28@gmail.com>
2021-09-08 23:52:23 +0900

    ルーティングの設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const About = { template: '<div>About</div>' };
+import FavoriteList from '../views/FavoriteList.vue';
+import FinancialResultsDetails from '../views/FinancialResultsDetails.vue';
+import Home from '../views/Home.vue';
+import UserFavoriteList from '../views/UserFavoriteList.vue';
+import UserWatchLaterList from '../views/UserWatchLaterList.vue';
+import WatchLaterList from '../views/WatchLaterList.vue';
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/about', component: About }],
+  routes: [
+    { path: '/favorite', component: FavoriteList },
+    { path: '/financial', component: FinancialResultsDetails },
+    { path: '/home', component: Home },
+    { path: '/userfavorite', component: UserFavoriteList },
+    { path: '/userwatchlater', component: UserWatchLaterList },
+    { path: '/watchlater', component: WatchLaterList },
+  ],
 });
