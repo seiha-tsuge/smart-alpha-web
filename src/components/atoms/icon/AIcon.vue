b605a4c35c56a777dd1f8eac236a06f1a6f83249
Seiha Tsuge <49358142+seiha-tsuge@users.noreply.github.com>
2021-09-15 21:41:51 +0900

    refactor: script setup記法に変更 (#56)

--- src/components/atoms/icon/AIcon.vue
+++ src/components/atoms/icon/AIcon.vue
@@ -4,28 +4,25 @@
   </i>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import type { PropType } from 'vue';
-import { defineComponent } from 'vue';
+import { defineProps } from 'vue';
 
 type Color = 'primary' | 'default';
 type Size = 'default' | 'small' | 'large';
 
-export default defineComponent({
-  name: 'AIcon',
-  props: {
-    icon: {
-      type: String as PropType<string>,
-      required: true,
-    },
-    color: {
-      type: String as PropType<Color>,
-      default: 'default',
-    },
-    size: {
-      type: String as PropType<Size>,
-      default: 'small',
-    },
+defineProps({
+  icon: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  color: {
+    type: String as PropType<Color>,
+    default: 'default',
+  },
+  size: {
+    type: String as PropType<Size>,
+    default: 'small',
   },
 });
 </script>
