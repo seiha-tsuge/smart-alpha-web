34a82b5de538c7e9085d42f10507eb7c970605d0
Takuya-Akutsu <potatop0103@gmail.com>
2021-09-20 12:32:47 +0900

    feat: Sidebar Layout修正

--- src/components/layout/LayoutSidebar.vue
+++ src/components/layout/LayoutSidebar.vue
@@ -1,13 +1,74 @@
 <template>
-  <Sidebar><slot /></Sidebar>
+  <aside :class="$style.sidebar">
+    <div :class="$style.items">
+      <div :class="$style.item">
+        <AIcon :icon="'business'" />
+        <p>決算速報</p>
+      </div>
+    </div>
+    <div :class="$style.items">
+      <div :class="$style.item">
+        <AIcon :icon="'star_border'" />
+        <p>お気に入り</p>
+      </div>
+      <div :class="$style.item">
+        <AIcon :icon="'schedule'" />
+        <p>後で見る</p>
+      </div>
+    </div>
+    <div :class="$style.items">
+      <div :class="$style.label">
+        <p>他のユーザー</p>
+      </div>
+      <div :class="$style.item">
+        <AIcon :icon="'star_border'" />
+        <p>お気に入り</p>
+      </div>
+      <div :class="$style.item">
+        <AIcon :icon="'schedule'" />
+        <p>後で見る</p>
+      </div>
+    </div>
+  </aside>
 </template>
 
 <script setup lang="ts">
-import Sidebar from '../navigation/Sidebar.vue';
+import AIcon from '../atoms/icon/AIcon.vue';
 </script>
 
 <style module>
 .sidebar {
-  grid-area: sidebar;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-white);
+}
+
+.items {
+  border-bottom: var(--border);
+  padding-left: 1.2rem;
+}
+
+.item {
+  font-size: 1.4rem;
+  padding: 1.2rem;
+  display: flex;
+  column-gap: 2.4rem;
+}
+
+.label {
+  display: flex;
+  column-gap: 2.4rem;
+  font-size: 1.2rem;
+  padding: 1.2rem;
+}
+
+@media screen and (max-width: 576px) {
+  .sidebar {
+    position: fixed;
+    top: 0;
+    z-index: var(--z-index-sidebar);
+    width: 75%;
+    height: 100vh;
+  }
 }
 </style>
--- src/components/navigation/Sidebar.vue
+++ src/components/navigation/Sidebar.vue
@@ -1,77 +1,13 @@
 <template>
-  <aside :class="$style.sidebar">
-    <div :class="$style.column">
-      <div />
-      <AIcon :icon="'business'" />
-      <p>決算速報</p>
-    </div>
-    <div :class="$style.border" />
-    <div :class="$style.column">
-      <div />
-      <AIcon :icon="'star_border'" />
-      <p>お気に入り</p>
-    </div>
-    <div :class="$style.column">
-      <div />
-      <AIcon :icon="'schedule'" />
-      <p>後で見る</p>
-    </div>
-    <div :class="$style.border" />
-    <div :class="$style.label">
-      <div />
-      <p>他のユーザー</p>
-    </div>
-    <div :class="$style.column">
-      <div />
-      <AIcon :icon="'star_border'" />
-      <p>お気に入り</p>
-    </div>
-    <div :class="$style.column">
-      <div />
-      <AIcon :icon="'schedule'" />
-      <p>後で見る</p>
-    </div>
-    <div :class="$style.border" />
-  </aside>
+  <LayoutSidebar :class="$style.header"><slot /></LayoutSidebar>
 </template>
 
 <script setup lang="ts">
-import AIcon from '../atoms/icon/AIcon.vue';
+import LayoutSidebar from '../layout/LayoutSidebar.vue';
 </script>
 
 <style module>
 .sidebar {
-  display: flex;
-  flex-direction: column;
-  row-gap: 1.2rem;
-  column-gap: 1.2rem;
-  font-size: 1.4rem;
-  background-color: var(--color-white);
-}
-
-.border {
-  border: var(--border);
-}
-
-.column {
-  display: flex;
-  grid-template-columns: auto;
-  column-gap: 2.4rem;
-}
-
-.label {
-  display: flex;
-  column-gap: 2.4rem;
-  font-size: 1.2rem;
-}
-
-@media screen and (max-width: 576px) {
-  .sidebar {
-    position: fixed;
-    top: 0;
-    z-index: var(--z-index-sidebar);
-    width: 75%;
-    height: 100vh;
-  }
+  grid-area: sidebar;
 }
 </style>
