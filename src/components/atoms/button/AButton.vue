1a2c7925d18353a4c7a4f3acff648d43161de46e
Reon Horigami <excalibr28@gmail.com>
2021-09-21 23:51:09 +0900

    Co-authored-by: Seiha Tsuge <seiha-tsuge@users.noreply.github.com>

--- src/components/atoms/button/AButton.vue
+++ src/components/atoms/button/AButton.vue
@@ -1,7 +1,17 @@
 <template>
-  <button>
+  <button @click="onClick">
     <slot />
   </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+
+const emit = defineEmits<{
+  (event: 'click', e: Event): void;
+}>();
+
+const onClick = (e: Event) => {
+  emit('click', e);
+};
+</script>
