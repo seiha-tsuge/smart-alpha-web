e2d7f980181446747be4236641c66981db61884e
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-09-24 23:52:38 +0900

    feat: AppLinkコンポーネントを作成

---
+++ src/components/AppLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link :to="{ name }">
+    <slot />
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import { defineProps } from 'vue';
+
+import type { RouteNames } from '../router/types';
+
+defineProps({
+  name: {
+    type: String as PropType<RouteNames>,
+    required: true,
+  },
+});
+</script>
