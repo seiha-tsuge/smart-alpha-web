72d9280fcf510b48b4cadba630b84e21a6fb33e5
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-09-25 00:02:52 +0900

    feat: paramsを追加

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="{ name }">
+  <router-link :to="{ name, params }">
     <slot />
   </router-link>
 </template>
@@ -8,12 +8,17 @@
 import type { PropType } from 'vue';
 import { defineProps } from 'vue';
 
+import type { RouteParams } from 'vue-router';
 import type { RouteNames } from '../router/types';
 
 defineProps({
   name: {
     type: String as PropType<RouteNames>,
     required: true,
+  },
+  params: {
+    type: Object as PropType<RouteParams>,
+    default: () => ({}),
   },
 });
 </script>
