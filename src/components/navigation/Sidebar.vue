fa2ed20c85b033fc7948d0c4da67bff08774c77f
Takuya-Akutsu <potatop0103@gmail.com>
2021-10-06 23:24:51 +0900

    refactor: AntDesignでのレイアウト適用

--- src/components/navigation/Sidebar.vue
+++ src/components/navigation/Sidebar.vue
@@ -1,46 +1,57 @@
 <template>
-  <aside :class="$style.sidebar">
-    <div :class="$style.items">
-      <router-link :to="{ name: 'FinancialResults' }" :class="$style.item">
-        <AIcon :icon="'business'" />
-        <p>決算速報</p>
-      </router-link>
-    </div>
-    <div :class="$style.items">
-      <router-link :to="{ name: 'FavoriteList' }" :class="$style.item">
-        <AIcon :icon="'star_border'" />
-        <p>お気に入り</p>
-      </router-link>
-      <router-link :to="{ name: 'WatchLaterList' }" :class="$style.item">
-        <AIcon :icon="'schedule'" />
-        <p>後で見る</p>
-      </router-link>
-    </div>
-    <div :class="$style.items">
-      <div :class="$style.item">
+  <a-layout-sider
+    v-model:collapsed="collapsed"
+    :class="$style.sidebar"
+    :trigger="null"
+    collapsible
+  >
+    <a-menu>
+      <a-menu-item :class="$style.items">
+        <router-link :to="{ name: 'FinancialResults' }" :class="$style.item">
+          <AIcon :icon="'business'" />
+          <p>決算速報</p>
+        </router-link>
+      </a-menu-item>
+      <a-menu-item :class="$style.items">
+        <router-link :to="{ name: 'FavoriteList' }" :class="$style.item">
+          <AIcon :icon="'star_border'" />
+          <p>お気に入り</p>
+        </router-link>
+      </a-menu-item>
+      <a-menu-item>
+        <router-link :to="{ name: 'WatchLaterList' }" :class="$style.item">
+          <AIcon :icon="'schedule'" />
+          <p>後で見る</p>
+        </router-link>
+      </a-menu-item>
+      <a-menu-item :class="$style.item">
         <p :class="$style.label">他のユーザー</p>
-      </div>
-      <router-link :to="{ name: 'UserFavoriteList' }" :class="$style.item">
-        <AIcon :icon="'star_border'" />
-        <p>お気に入り</p>
-      </router-link>
-      <router-link :to="{ name: 'UserWatchLaterList' }" :class="$style.item">
-        <AIcon :icon="'schedule'" />
-        <p>後で見る</p>
-      </router-link>
-    </div>
-  </aside>
+      </a-menu-item>
+      <a-menu-item>
+        <router-link :to="{ name: 'UserFavoriteList' }" :class="$style.item">
+          <AIcon :icon="'star_border'" />
+          <p>お気に入り</p>
+        </router-link>
+      </a-menu-item>
+      <a-menu-item>
+        <router-link :to="{ name: 'UserWatchLaterList' }" :class="$style.item">
+          <AIcon :icon="'schedule'" />
+          <p>後で見る</p>
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
 </template>
 
 <script setup lang="ts">
-import AIcon from '../atoms/icon/AIcon.vue';
+import AIcon from '../general/icon/AIcon.vue';
 </script>
 
 <style module>
 .sidebar {
-  grid-area: sidebar;
   display: flex;
   flex-direction: column;
+  grid-area: sidebar;
   background-color: var(--color-white);
 }
 
@@ -49,10 +60,10 @@ import AIcon from '../atoms/icon/AIcon.vue';
 }
 
 .item {
-  font-size: 1.4rem;
-  padding: 1.2rem 2.4rem;
   display: flex;
   column-gap: 2.4rem;
+  padding: 1.2rem 2.4rem;
+  font-size: 1.4rem;
 }
 
 .label {
