3a8af0ca683a5a7e23fc8e6cd78007400550e80f
seiha-tsuge <seiha.tsuge@gmail.com>
2023-03-02 00:06:23 +0900

    feat: Noto_Sans_JPを導入

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -2,13 +2,25 @@ import * as React from 'react';
 
 import { MantineProvider } from '@mantine/core';
 
+import { Noto_Sans_JP } from '@next/font/google';
+
+const NotoSansJP = Noto_Sans_JP({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
+const fontFamily = `${NotoSansJP.style.fontFamily}, sans-serif`;
+
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        fontFamily,
+        headings: { fontFamily },
+      }}
+    >
       {children}
     </MantineProvider>
   );
