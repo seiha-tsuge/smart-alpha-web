5d6bd76044ad46cf8eaf9627116388c169b2cb20
Seiha Tsuge <49358142+seiha-tsuge@users.noreply.github.com>
2023-06-21 23:36:28 +0900

    feat: MainLayoutにスタイルを設定 (#101)

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -5,14 +5,17 @@ import { AppShell } from '@/components/shared';
 import { Header } from './Header';
 import { Navbar } from './Navbar';
 
+import { useStyles } from './MainLayout.styles';
+
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
+  const { classes } = useStyles();
   return (
-    <AppShell navbar={<Navbar />} header={<Header />}>
-      {children}
+    <AppShell navbar={<Navbar />} header={<Header />} padding={0}>
+      <div className={classes.container}>{children}</div>
     </AppShell>
   );
 };
