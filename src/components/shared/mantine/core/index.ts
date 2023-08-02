fecb7b49c2990ae0c44fb398f5a1261a4230a69e
seiha-tsuge <seiha.tsuge@gmail.com>
2023-08-02 22:00:02 +0900

    feat: Textコンポーネントを作成

--- src/components/shared/mantine/core/index.ts
+++ src/components/shared/mantine/core/index.ts
@@ -3,5 +3,6 @@ export * from './AppShell';
 export * from './Flex';
 export * from './Group';
 export * from './NavLink';
+export * from './Text';
 export * from './Tooltip';
 export * from './UnstyledButton';
